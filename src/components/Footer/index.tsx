72e7ac83e5d8044ad971835dd8a6d77989d1d19c
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-11-15 15:33:55 -0300

    feat: Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className='flex justify-between py-4 px-6 bg-zinc-800'>
+      <span>Global Solution</span>
+      <span>© 2024</span>
+    </footer>
+  );
+}
+
+export default Footer;
